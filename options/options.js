f87803cea419d67f21798e11411082352446449b
Crowdbotics <team@crowdbotics.com>
2023-11-18 04:05:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-meadow-44657",
+  url: "https://royal-meadow-44657.botics.co",
+  api: "https://royal-meadow-44657.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
